1262d5e763845467b41e0a4f336fbff8f9916bdb
shaikaziya <shaikaziya1234@gmail.com>
2023-01-15 19:12:51 +0530

    adde css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-
+import { Button } from "@mui/material";
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -14,7 +15,9 @@ export default function Navbar() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Users Information
             </Typography>
-            <Link to="/create-user">Add User</Link>
+            <Link className="button" to="/create-user">
+              Add User
+            </Link>
           </Toolbar>
         </AppBar>
       </Box>
